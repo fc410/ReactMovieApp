11b3f669e876a2f1f8bcee21e085c18bd9a03b4a
fc410 <fc410@humboldt.edu>
2022-06-24 16:32:20 -0700

    Header component
    Fully functional header components. Can also be accessed in smaller devices like a phone.

---
+++ app/src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import RMDBLogo from '../../images/react-movie-logo.svg';
+import TMBDLogo from '../../images/tmdb_logo.svg';
+
+import { Wrapper, Content, LogoImg, TMBDLogoImg } from './Header.styles';
+
+const Header = () => (
+    <Wrapper>
+        <Content>
+            <LogoImg src={RMDBLogo} alt='rmdb-logo' />
+            <TMBDLogoImg src={TMBDLogo} alt='tmdb-logo' />
+        </Content>
+    </Wrapper>
+)
+
+export default Header;
